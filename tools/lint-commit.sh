bffdbf5e0c9682974fc5b5644d03e5ae058ded3b
nilsrothe <nils.rothe@healthinal.com>
2024-05-19 18:18:02 +0200

    feat(HOME-0): added commit-hooks and grouped dependabot updates

---
+++ tools/lint-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+jira="HOME"
+scope_regex="(deps|deps-dev|${jira}-[0-9]+)"
+types="build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test"
+message_regex="^(${types})\(${scope_regex}\): [a-zA-Z]"
+
+if ! [[ "$1" =~ $message_regex ]]; then
+  echo
+  echo "Invalid commit message: $1"
+  echo "Rules:"
+  echo " ◆ Format: <type>(<scope>): <description>"
+  echo " ◆ Types: ${types//|/, }"
+  echo " ◆ Scope: ${jira} ticket number"
+  echo
+  exit 1
+fi
